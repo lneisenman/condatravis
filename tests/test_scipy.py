37554c7ed52eaffced8aef07293517412c2d9d7d
Larry Eisenman <leisenman@wustl.edu>
2015-04-16 19:59:46 -0500

    add stuff

---
+++ tests/test_scipy.py
@@ -0,0 +1,6 @@
+import numpy as np
+import scipy as sp
+
+
+def test_silly():
+    assert 1 == 1
--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -11,30 +11,18 @@
 set -e
 
 sudo apt-get update -qq
-sudo apt-get install git
+sudo apt-get -qq install git
 
-if [[ "$DISTRIB" == "conda" ]]; then
-    # Deactivate the travis-provided virtual environment and setup a
-    # conda-based environment instead
-    deactivate
 
-    # Use the miniconda installer for faster download / install of conda
-    # itself
-    wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh \
-        -O miniconda.sh
-    chmod +x miniconda.sh && ./miniconda.sh -b
-    export PATH=/home/travis/miniconda/bin:$PATH
-    conda update --yes conda
+# Use the miniconda installer for faster download / install of conda itself
+wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
+chmod +x miniconda.sh
+./miniconda.sh -b
+export PATH=/home/travis/miniconda/bin:$PATH
+conda update --yes conda
 
-    # Configure the conda environment and put it in the path using the
-    # provided versions
-    conda create -n testenv --yes python=$PYTHON_VERSION pip
-    source activate testenv
+conda install --yes python=$PYTHON_VERSION pip atlas numpy scipy
 
-elif [[ "$DISTRIB" == "ubuntu" ]]; then
-    # Use standard ubuntu packages in their default version
-    sudo apt-get install -qq python-pip
-fi
 
 if [[ "$COVERAGE" == "true" ]]; then
     pip install coverage coveralls
